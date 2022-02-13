3851bb11d668676d395b085450873e153e54dc16
RealHath <1203483380@qq.com>
2022-02-13 21:52:46 +0800

    web server

---
+++ buildup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# 环境配置脚本
+
+cd ~
+
+# 强迫症更新
+yum -y upgrade
+
+# gcc版本升级
+sudo yum -y install gcc
+sudo yum -y install gcc-c++
+sudo yum -y install centos-release-scl
+sudo yum -y install devtoolset-10-gcc*
+source /opt/rh/devtoolset-10/enable
+mv /usr/bin/gcc /usr/bin/gcc-4.8.5
+ln -s /opt/rh/devtoolset-10/root/bin/gcc /usr/bin/gcc
+mv /usr/bin/g++ /usr/bin/g++-4.8.5
+ln -s /opt/rh/devtoolset-10/root/bin/g++ /usr/bin/g++
+
+# 编译工具
+sudo yum -y install make
+sudo yum -y install cmake
+
+# 安装brpc https://github.com/apache/incubator-brpc/blob/master/docs/cn/getting_started.md#fedoracentos
+cd ~
+
+sudo yum -y install epel-release
+sudo yum -y install git openssl-devel
+sudo yum -y install gflags-devel protobuf-devel protobuf-compiler leveldb-devel
+sudo yum -y install gperftools-devel
+sudo yum -y install gtest-devel
+sudo yum -y install glog-devel
+
+git clone git://github.com/apache/incubator-brpc.git
+cd incubator-brpc/
+sh config_brpc.sh --headers="/usr/include" --libs="/usr/lib64 /usr/bin" --nodebugsymbols --with-glog
+make
+
+# mysql
+sudo yum -y install mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+# mysql_secure_installation配置mysql  https://www.cnblogs.com/yhongji/p/9783065.html
+sudo yum -y install mysql-devel
+
+# 安装杂项
+sudo yum -y install zip
--- test/test.cpp
+++ test/test.cpp
@@ -7,61 +7,161 @@ extern "C"
 #include <gflags/gflags.h>
 #include <butil/logging.h>
 #include <brpc/server.h>
+#include <brpc/restful.h>
 #include "test.pb.h"
-#include "module.h"
+#include "test2.pb.h"
 
-DEFINE_bool(echo_attachment, true, "Echo attachment as well");
-DEFINE_int32(port, 8000, "TCP Port of this server");
+DEFINE_int32(port, 8010, "TCP Port of this server");
 DEFINE_int32(idle_timeout_s, -1, "Connection will be closed if there is no "
                                  "read/write operations during the last `idle_timeout_s'");
 DEFINE_int32(logoff_ms, 2000, "Maximum duration of server's LOGOFF state "
                               "(waiting for client to close connection before server stops)");
 
-// Your implementation of example::EchoService
-// Notice that implementing brpc::Describable grants the ability to put
-// additional information in /status.
+DEFINE_string(certificate, "cert.pem", "Certificate file path to enable SSL");
+DEFINE_string(private_key, "key.pem", "Private key file path to enable SSL");
+DEFINE_string(ciphers, "", "Cipher suite used for SSL connections");
+
 namespace example
 {
-    class EchoServiceImpl : public EchoService
+
+    // Service with static path.
+    class HttpServiceImpl : public HttpService
     {
     public:
-        EchoServiceImpl(){};
-        virtual ~EchoServiceImpl(){};
-        virtual void Echo(google::protobuf::RpcController *cntl_base,
-                          const EchoRequest *request,
-                          EchoResponse *response,
-                          google::protobuf::Closure *done)
+        HttpServiceImpl(){};
+        virtual ~HttpServiceImpl(){};
+        void Echo(google::protobuf::RpcController *cntl_base,
+                  const HttpRequest *,
+                  HttpResponse *,
+                  google::protobuf::Closure *done)
         {
             // This object helps you to call done->Run() in RAII style. If you need
             // to process the request asynchronously, pass done_guard.release().
-            // 保留
             brpc::ClosureGuard done_guard(done);
 
-            // cntl句柄，有前端的信息
             brpc::Controller *cntl =
                 static_cast<brpc::Controller *>(cntl_base);
-
-            // The purpose of following logs is to help you to understand
-            // how clients interact with servers more intuitively. You should
-            // remove these logs in performance-sensitive servers.
-            LOG(INFO) << "Received request[log_id=" << cntl->log_id()
-                      << "] from " << cntl->remote_side()
-                      << " to " << cntl->local_side()
-                      << ": " << request->message()
-                      << " (attached=" << cntl->request_attachment() << ")";
-
             // Fill response.
-            response->set_message(request->message());
-
-            // You can compress the response by setting Controller, but be aware
-            // that compression may be costly, evaluate before turning on.
-            // cntl->set_response_compress_type(brpc::COMPRESS_TYPE_GZIP);
-
-            if (FLAGS_echo_attachment)
+            cntl->http_response().set_content_type("text/plain");
+            butil::IOBufBuilder os;
+            os << "queries:";
+            for (brpc::URI::QueryIterator it = cntl->http_request().uri().QueryBegin();
+                 it != cntl->http_request().uri().QueryEnd(); ++it)
             {
-                // Set attachment which is wired to network directly instead of
-                // being serialized into protobuf messages.
-                cntl->response_attachment().append(cntl->request_attachment());
+                os << ' ' << it->first << '=' << it->second;
+            }
+            os << "\nbody: " << cntl->request_attachment() << '\n';
+            LOG(INFO) << "HttpServiceImpl Echo" << os.buf();
+            os.move_to(cntl->response_attachment());
+        }
+    };
+
+    // Service with dynamic path.
+    class FileServiceImpl : public FileService
+    {
+    public:
+        FileServiceImpl(){};
+        virtual ~FileServiceImpl(){};
+
+        struct Args
+        {
+            butil::intrusive_ptr<brpc::ProgressiveAttachment> pa;
+        };
+
+        static void *SendLargeFile(void *raw_args)
+        {
+            std::unique_ptr<Args> args(static_cast<Args *>(raw_args));
+            if (args->pa == NULL)
+            {
+                LOG(ERROR) << "ProgressiveAttachment is NULL";
+                return NULL;
+            }
+            for (int i = 0; i < 100; ++i)
+            {
+                char buf[16];
+                int len = snprintf(buf, sizeof(buf), "part_%d ", i);
+                args->pa->Write(buf, len);
+
+                // sleep a while to send another part.
+                bthread_usleep(10000);
+            }
+            return NULL;
+        }
+
+        void default_method(google::protobuf::RpcController *cntl_base,
+                            const HttpRequest *,
+                            HttpResponse *,
+                            google::protobuf::Closure *done)
+        {
+            brpc::ClosureGuard done_guard(done);
+            brpc::Controller *cntl =
+                static_cast<brpc::Controller *>(cntl_base);
+            const std::string &filename = cntl->http_request().unresolved_path();
+            LOG(INFO) << "FileService default_method";
+            if (filename == "largefile")
+            {
+                // Send the "largefile" with ProgressiveAttachment.
+                std::unique_ptr<Args> args(new Args);
+                args->pa = cntl->CreateProgressiveAttachment();
+                bthread_t th;
+                bthread_start_background(&th, NULL, SendLargeFile, args.release());
+            }
+            else
+            {
+                cntl->response_attachment().append("Getting file: ");
+                cntl->response_attachment().append(filename);
+            }
+        }
+    };
+
+    // Restful service. (The service implementation is exactly same with regular
+    // services, the difference is that you need to pass a `restful_mappings'
+    // when adding the service into server).
+    class QueueServiceImpl : public example::QueueService
+    {
+    public:
+        QueueServiceImpl(){};
+        virtual ~QueueServiceImpl(){};
+        void start(google::protobuf::RpcController *cntl_base,
+                   const HttpRequest *,
+                   HttpResponse *,
+                   google::protobuf::Closure *done)
+        {
+            brpc::ClosureGuard done_guard(done);
+            brpc::Controller *cntl =
+                static_cast<brpc::Controller *>(cntl_base);
+            cntl->response_attachment().append("queue started");
+            LOG(INFO) << "QueueService start";
+        }
+        void stop(google::protobuf::RpcController *cntl_base,
+                  const HttpRequest *,
+                  HttpResponse *,
+                  google::protobuf::Closure *done)
+        {
+            brpc::ClosureGuard done_guard(done);
+            brpc::Controller *cntl =
+                static_cast<brpc::Controller *>(cntl_base);
+            cntl->response_attachment().append("queue stopped");
+            LOG(INFO) << "QueueService start";
+        }
+        void getstats(google::protobuf::RpcController *cntl_base,
+                      const HttpRequest *,
+                      HttpResponse *,
+                      google::protobuf::Closure *done)
+        {
+            LOG(INFO) << "QueueService getstats";
+            brpc::ClosureGuard done_guard(done);
+            brpc::Controller *cntl =
+                static_cast<brpc::Controller *>(cntl_base);
+            const std::string &unresolved_path = cntl->http_request().unresolved_path();
+            if (unresolved_path.empty())
+            {
+                cntl->response_attachment().append("Require a name after /stats");
+            }
+            else
+            {
+                cntl->response_attachment().append("Get stats: ");
+                cntl->response_attachment().append(unresolved_path);
             }
         }
     };
@@ -91,27 +191,47 @@ int main(int argc, char *argv[])
     brpc::Server server;
 
     // Instance of your service.
-    example::EchoServiceImpl echo_service_impl;
+    example::HttpServiceImpl http_svc;
+    example::FileServiceImpl file_svc;
+    example::QueueServiceImpl queue_svc;
 
-    // Add the service into server. Notice the second parameter, because the
+    // Add services into server. Notice the second parameter, because the
     // service is put on stack, we don't want server to delete it, otherwise
     // use brpc::SERVER_OWNS_SERVICE.
-    if (server.AddService(&echo_service_impl,
+    if (server.AddService(&http_svc,
                           brpc::SERVER_DOESNT_OWN_SERVICE) != 0)
     {
-        LOG(ERROR) << "Fail to add service";
+        LOG(ERROR) << "Fail to add http_svc";
+        return -1;
+    }
+    if (server.AddService(&file_svc,
+                          brpc::SERVER_DOESNT_OWN_SERVICE) != 0)
+    {
+        LOG(ERROR) << "Fail to add file_svc";
+        return -1;
+    }
+    if (server.AddService(&queue_svc,
+                          brpc::SERVER_DOESNT_OWN_SERVICE,
+                          "/v1/queue/start   => start,"
+                          "/v1/queue/stop    => stop,"
+                          "/v1/queue/stats/* => getstats") != 0)
+    {
+        LOG(ERROR) << "Fail to add queue_svc";
         return -1;
     }
 
     // Start the server.
     brpc::ServerOptions options;
     options.idle_timeout_sec = FLAGS_idle_timeout_s;
+    // options.mutable_ssl_options()->default_cert.certificate = FLAGS_certificate;
+    // options.mutable_ssl_options()->default_cert.private_key = FLAGS_private_key;
+    // options.mutable_ssl_options()->ciphers = FLAGS_ciphers;
     if (server.Start(FLAGS_port, &options) != 0)
     {
-        LOG(ERROR) << "Fail to start EchoServer";
+        LOG(ERROR) << "Fail to start HttpServer";
         return -1;
     }
-
+    LOG(INFO) << "http server start sucess";
     // Wait until Ctrl-C is pressed, then Stop() and Join() the server.
     server.RunUntilAskedToQuit();
     return 0;
