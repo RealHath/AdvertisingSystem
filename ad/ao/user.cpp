085b7c6114151385734edad37109b01f239a04d3
RealHath <1203483380@qq.com>
2022-03-10 16:00:58 +0800

    transaction try catch

--- ad/ao/user.cpp
+++ ad/ao/user.cpp
@@ -1,6 +1,7 @@
 #include "user.h"
 #include "sql.h"
 #include "errorEnum.pb.h"
+#include <butil/logging.h>
 
 using namespace mysqlpp;
 
@@ -30,17 +31,24 @@ namespace ad_namespace
             tran(*conn, Transaction::IsolationLevel::repeatable_read,
                  Transaction::IsolationScope::session);
 
-        char buf[2048];
-        sprintf(buf, "UPDATE money SET amount=%lf, welfare=%lf, updateTime=%lu WHERE uuid='%s';",
-                amount, welfare, time(NULL), uuid.c_str());
-        string sql(buf);
-        MyDB::getInstance()->execSQL(sql);
+        try
+        {
+            char buf[2048];
+            sprintf(buf, "UPDATE money SET amount=%lf, welfare=%lf, updateTime=%lu WHERE uuid='%s';",
+                    amount, welfare, time(NULL), uuid.c_str());
+            string sql(buf);
+            MyDB::getInstance()->execSQL(sql);
 
-        memset(buf, 0, 2048);
-        sprintf(buf, "INSERT INTO recharge(id,uuid,increment,updateTime) VALUES(null,'%s',%lf,%lu);",
-                uuid.c_str(), increment, time(NULL));
-        MyDB::getInstance()->execSQL(string(buf));
-        tran.commit();
+            memset(buf, 0, 2048);
+            sprintf(buf, "INSERT INTO recharge(id,uuid,increment,updateTime) VALUES(null,'%s',%lf,%lu);",
+                    uuid.c_str(), increment, time(NULL));
+            MyDB::getInstance()->execSQL(string(buf));
+            tran.commit();
+        }
+        catch (std::exception &e)
+        {
+            tran.rollback();
+        }
     }
 
     // 插入用户
@@ -51,17 +59,25 @@ namespace ad_namespace
             tran(*conn, Transaction::IsolationLevel::repeatable_read,
                  Transaction::IsolationScope::session);
 
-        char buf[2048];
-        sprintf(buf, "INSERT INTO user(id,username,password,phone,uuid,registTime) VALUES(null,'%s','%s',%lu,'%s',%lu);",
-                username.c_str(), password.c_str(), phone, uuid.c_str(), time(NULL));
-        string sql(buf);
-        MyDB::getInstance()->execSQL(sql);
-        memset(buf, 0, 2048);
-        sprintf(buf, "INSERT INTO money(id,uuid,amount,welfare,updateTime) VALUES(null,'%s',%lf,%lf,%lu);",
-                uuid.c_str(), amount, welfare, time(NULL));
-        sql = string(buf);
-        MyDB::getInstance()->execSQL(sql);
-        tran.commit();
+        try
+        {
+            char buf[2048];
+            sprintf(buf, "INSERT INTO user(id,username,password,phone,uuid,registTime) VALUES(null,'%s','%s',%lu,'%s',%lu);",
+                    username.c_str(), password.c_str(), phone, uuid.c_str(), time(NULL));
+            string sql(buf);
+            MyDB::getInstance()->execSQL(sql);
+            memset(buf, 0, 2048);
+            sprintf(buf, "INSERT INTO money(id,uuid,amount,welfare,updateTime) VALUES(null,'%s',%lf,%lf,%lu);",
+                    uuid.c_str(), amount, welfare, time(NULL));
+            sql = string(buf);
+            MyDB::getInstance()->execSQL(sql);
+            tran.commit();
+        }
+        catch (std::exception &e)
+        {
+            LOG(ERROR) << "updateMoney rollback";
+            tran.rollback();
+        }
     }
 
     //--------------------
@@ -82,109 +98,133 @@ namespace ad_namespace
         mysqlpp::Transaction
             tran(*conn, Transaction::IsolationLevel::repeatable_read,
                  Transaction::IsolationScope::session);
-        char buf[2048];
-        sprintf(buf, "INSERT INTO ad(id,uuid,type,imageUrl,url,content,updateTime) VALUES(%u,'%s',%u,'%s','%s','%s',%lu);",
-                id, uuid.c_str(), type, imageUrl.c_str(), url.c_str(), content.c_str(), time(NULL));
-        string sql = string(buf);
-        MyDB::getInstance()->execSQL(sql);
 
-        memset(buf, 0, 2048);
-        if (type == errorEnum::ADType::CLICK)
+        try
         {
-            sprintf(buf, "INSERT INTO adFlow(id,uuid,click,exposure,updateTime) VALUES(%u,'%s',0,0,%lu);",
-                    id, uuid.c_str(), time(NULL));
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::MILLE)
-        {
-            sprintf(buf, "INSERT INTO adFlow(id,uuid,click,exposure,updateTime) VALUES(%u,'%s',0,0,%lu);",
-                    id, uuid.c_str(), time(NULL));
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::VISIT)
-        {
-            sprintf(buf, "INSERT INTO adFlow(id,uuid,visit,shop,sell,updateTime) VALUES(%u,'%s',0,0,0,%lu);",
-                    id, uuid.c_str(), time(NULL));
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::SHOP)
-        {
-            sprintf(buf, "INSERT INTO adFlow(id,uuid,visit,shop,sell,updateTime) VALUES(%u,'%s',0,0,0,%lu);",
-                    id, uuid.c_str(), time(NULL));
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::SELL)
-        {
-            sprintf(buf, "INSERT INTO adFlow(id,uuid,visit,shop,sell,updateTime) VALUES(%u,'%s',0,0,0,%lu);",
-                    id, uuid.c_str(), time(NULL));
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::TIME)
-        {
-            sprintf(buf, "INSERT INTO adFlow(id,uuid,startTime,endTime) VALUES(%u,'%s',%lu,%lu);",
-                    id, uuid.c_str(), time(NULL), time(NULL) + 3600);
-            sql = string(buf);
-        }
-        else
-        {
-            sql.clear();
-        }
-        if (sql.size())
-        {
+            char buf[2048];
+            sprintf(buf, "INSERT INTO ad(id,uuid,type,imageUrl,url,content,updateTime) VALUES(%u,'%s',%u,'%s','%s','%s',%lu);",
+                    id, uuid.c_str(), type, imageUrl.c_str(), url.c_str(), content.c_str(), time(NULL));
+            string sql = string(buf);
             MyDB::getInstance()->execSQL(sql);
-        }
 
-        tran.commit();
+            memset(buf, 0, 2048);
+            if (type == errorEnum::ADType::CLICK)
+            {
+                sprintf(buf, "INSERT INTO adFlow(id,uuid,click,exposure,updateTime) VALUES(%u,'%s',0,0,%lu);",
+                        id, uuid.c_str(), time(NULL));
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::MILLE)
+            {
+                sprintf(buf, "INSERT INTO adFlow(id,uuid,click,exposure,updateTime) VALUES(%u,'%s',0,0,%lu);",
+                        id, uuid.c_str(), time(NULL));
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::VISIT)
+            {
+                sprintf(buf, "INSERT INTO adFlow(id,uuid,visit,shop,sell,updateTime) VALUES(%u,'%s',0,0,0,%lu);",
+                        id, uuid.c_str(), time(NULL));
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::SHOP)
+            {
+                sprintf(buf, "INSERT INTO adFlow(id,uuid,visit,shop,sell,updateTime) VALUES(%u,'%s',0,0,0,%lu);",
+                        id, uuid.c_str(), time(NULL));
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::SELL)
+            {
+                sprintf(buf, "INSERT INTO adFlow(id,uuid,visit,shop,sell,updateTime) VALUES(%u,'%s',0,0,0,%lu);",
+                        id, uuid.c_str(), time(NULL));
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::TIME)
+            {
+                sprintf(buf, "INSERT INTO adFlow(id,uuid,startTime,endTime) VALUES(%u,'%s',%lu,%lu);",
+                        id, uuid.c_str(), time(NULL), time(NULL) + 3600);
+                sql = string(buf);
+            }
+            else
+            {
+                sql.clear();
+            }
+            if (sql.size())
+            {
+                MyDB::getInstance()->execSQL(sql);
+            }
+
+            tran.commit();
+        }
+        catch (std::exception &e)
+        {
+            LOG(ERROR) << "insertAd rollback";
+            tran.rollback();
+        }
     }
 
     void Advertise::updateCost()
     {
-        char buf[2048];
-        string sql;
-        if (type == errorEnum::ADType::CLICK)
-        {
-            sprintf(buf, "UPDATE adFlow SET 'click' = 'click'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
-                    time(NULL), id, uuid.c_str());
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::MILLE)
-        {
-            sprintf(buf, "UPDATE adFlow SET 'exposure' = 'exposure'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
-                    time(NULL), id, uuid.c_str());
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::VISIT)
-        {
-            sprintf(buf, "UPDATE adAction SET 'visit' = 'visit'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
-                    time(NULL), id, uuid.c_str());
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::SHOP)
-        {
-            sprintf(buf, "UPDATE adAction SET 'shop' = 'shop'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
-                    time(NULL), id, uuid.c_str());
-            sql = string(buf);
-        }
-        else if (type == errorEnum::ADType::SELL)
-        {
-            sprintf(buf, "UPDATE adAction SET 'sell' = 'sell'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
-                    time(NULL), id, uuid.c_str());
-            sql = string(buf);
-        }
-        // else if (type == errorEnum::ADType::TIME)
-        // {
-        //     sprintf(buf, "UPDATE adFlow SET 'click' = 'click'+1 WHERE id=%u AND uuid='%s';",
-        //             id, uuid.c_str());
-        //     sql = string(buf);
-        // }
-        else
-        {
-            sql.clear();
-        }
+        auto conn = MyDB::getInstance()->getConnection();
+        mysqlpp::Transaction
+            tran(*conn, Transaction::IsolationLevel::repeatable_read,
+                 Transaction::IsolationScope::session);
 
-        if (sql.size())
+        try
         {
-            MyDB::getInstance()->execSQL(sql);
+            char buf[2048];
+            string sql;
+            if (type == errorEnum::ADType::CLICK)
+            {
+                sprintf(buf, "UPDATE adFlow SET 'click' = 'click'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
+                        time(NULL), id, uuid.c_str());
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::MILLE)
+            {
+                sprintf(buf, "UPDATE adFlow SET 'exposure' = 'exposure'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
+                        time(NULL), id, uuid.c_str());
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::VISIT)
+            {
+                sprintf(buf, "UPDATE adAction SET 'visit' = 'visit'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
+                        time(NULL), id, uuid.c_str());
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::SHOP)
+            {
+                sprintf(buf, "UPDATE adAction SET 'shop' = 'shop'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
+                        time(NULL), id, uuid.c_str());
+                sql = string(buf);
+            }
+            else if (type == errorEnum::ADType::SELL)
+            {
+                sprintf(buf, "UPDATE adAction SET 'sell' = 'sell'+1,'updateTime' = %lu WHERE id=%u AND uuid='%s';",
+                        time(NULL), id, uuid.c_str());
+                sql = string(buf);
+            }
+            // else if (type == errorEnum::ADType::TIME)
+            // {
+            //     sprintf(buf, "UPDATE adFlow SET 'click' = 'click'+1 WHERE id=%u AND uuid='%s';",
+            //             id, uuid.c_str());
+            //     sql = string(buf);
+            // }
+            else
+            {
+                sql.clear();
+            }
+
+            if (sql.size())
+            {
+                MyDB::getInstance()->execSQL(sql);
+            }
+
+            tran.commit();
+        }
+        catch (std::exception &e)
+        {
+            LOG(ERROR) << "updateCost rollback";
+            tran.rollback();
         }
     }
 }
